311221c0eb15c909ebb1dbc963184faa3b0bd1d8
chukwudiobianwu <chukwudiobianwu>
2023-05-24 01:23:52 -0700

    almost

--- whackamole.java
+++ whackamole.java
@@ -14,11 +14,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.*;
+import javafx.scene.control.ProgressBar;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 import java.util.Random;
+import javafx.scene.layout.AnchorPane;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 
 
 public class whackamole extends Application {
@@ -28,15 +31,21 @@ public class whackamole extends Application {
     private StackPane stackPane;
     private static final int BOARD_SIZE = 4; // Adjust this to change the board size
     private int scorecount; // Adjust this to change the cell size
+    private int seconds = 0;
 
     private static final int MOLE_APPEAR_TIME = 1000; // Adjust this to change mole appearance time (in milliseconds)
-    private static final int MOLE_DISAPPEAR_TIME = 50000; // Adjust this to change mole disappearance time (in milliseconds)
+    private static int MOLE_DISAPPEAR_TIME = 1000; // Adjust this to change mole disappearance time (in milliseconds)
+
+    private Timeline timeline = new Timeline();
 
     private ImageView [][] moles;
     private Rectangle inrect = new Rectangle();
     private Rectangle rect = new Rectangle();
     Image img = new Image("wmole.png");
     Random ran;
+
+    private VBox root3;
+    private Label timerLabel;
 
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
@@ -51,7 +60,16 @@ public class whackamole extends Application {
         imageView.fitWidthProperty().bind(primaryStage.widthProperty());
         imageView.fitHeightProperty().bind(primaryStage.heightProperty());
 
-        Text text = new Text();
+        Button backbButton =new Button("Back");
+        backbButton.setPrefWidth(200);
+        backbButton.setPrefHeight(50);
+        backbButton.setStyle("-fx-font-size: 20px; -fx-background-color: #873260; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
+
+        Button backbButto =new Button("Back");
+        backbButto.setPrefWidth(200);
+        backbButto.setPrefHeight(50);
+        backbButto.setStyle("-fx-font-size: 20px; -fx-background-color: #873260; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
+
         Button btn = new Button("Start Game");
         btn.setPrefWidth(200);
         btn.setPrefHeight(50);
@@ -63,29 +81,37 @@ public class whackamole extends Application {
         closeButton.setPrefHeight(50);
         closeButton.setStyle("-fx-font-size: 20px; -fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
 
-        text.setFont(new Font(45));
+
 
         VBox root = new VBox();
         root.setAlignment(Pos.BOTTOM_CENTER);
         root.setSpacing(20);
         VBox.setMargin(closeButton, new Insets(0, 0, 40, 0));
-        root.getChildren().addAll(text, btn, closeButton);
+        root.getChildren().addAll(btn, closeButton);
 
         stackPane = new StackPane();
         stackPane.getChildren().addAll(imageView, root);
+        
 
         Button levelButton = new Button("Select Difficulty");
         levelButton.setPrefWidth(200);
         levelButton.setPrefHeight(50);
         levelButton.setStyle("-fx-font-size: 20px; -fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
-
-        btn.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            VBox root2 = new VBox();
+           
+        VBox root2 = new VBox();
             root2.setAlignment(Pos.BOTTOM_CENTER);
             root2.setSpacing(20);
-            VBox.setMargin(levelButton, new Insets(0, 0, 40, 0));
-            root2.getChildren().addAll(levelButton);
+
+        btn.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+
+            VBox.setMargin(backbButton, new Insets(0, 0, 40, 0));
+            root2.getChildren().setAll(levelButton,backbButton);
             stackPane.getChildren().setAll(imageView, root2);
+            
+        }); 
+
+        backbButton.setOnMouseClicked(event -> {
+            mainmenue(imageView,root);
         });
 
         Button easy = new Button("Easy");
@@ -102,15 +128,25 @@ public class whackamole extends Application {
         hard.setPrefWidth(200);
         hard.setPrefHeight(50);
         hard.setStyle("-fx-font-size: 20px; -fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
+            
+        root3 = new VBox();
+        root3.setAlignment(Pos.BOTTOM_CENTER);
+        root3.setSpacing(20);
+
 
         levelButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            VBox root3 = new VBox();
-            root3.setAlignment(Pos.BOTTOM_CENTER);
-            root3.setSpacing(20);
-            VBox.setMargin(hard, new Insets(0, 0, 20, 0));
-            root3.getChildren().addAll(easy, medium, hard);
+
+            VBox.setMargin(backbButto, new Insets(0, 0, 20, 0));
+            root3.getChildren().setAll(easy, medium, hard, backbButto);
             stackPane.getChildren().setAll(imageView, root3);
+        
         });
+
+        backbButto.addEventHandler(MouseEvent.MOUSE_CLICKED,event -> {
+            levelmenue(imageView, root2);
+        });
+        
+
 
         easy.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             stackPane.getChildren().setAll(setBoardAndMoles()); // Pass board size as argument
@@ -156,8 +192,6 @@ public class whackamole extends Application {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
 
-        GridPane gridP = new GridPane();
-        gridP.setAlignment(Pos.CENTER);
     
         // Set the dimensions of each ellipse
         double ellipseWidth = inrect.getWidth() / 4;
@@ -183,12 +217,65 @@ public class whackamole extends Application {
                 // Reduce gaps around the border and edge of the rectangl
             }
         }
-    
+        
 
         moles = new ImageView[4][4];
         ran = new Random();
-        Text sCORE = new Text();  
-        Image moleImage = img;
+        Text sCORE = new Text("Score: " + scorecount + "/20");
+        sCORE.setFont(new Font(15));  
+        sCORE.setStyle("-fx-border-color: black;  -fx-text-fill: brown;");
+
+        ProgressBar progressBar = new ProgressBar();
+        progressBar.setMinWidth(200);
+        progressBar.setMaxWidth(300);
+        
+        Button backbBut =new Button("Back");
+        backbBut.setPrefWidth(90);
+        backbBut.setPrefHeight(50);
+        backbBut.setStyle("-fx-font-size: 20px; -fx-background-color: #873260; -fx-text-fill: #ffffff; -fx-effect:dropshadow(one-pass-box,black,10,10.10,2,0);");
+        
+        if(root3 == null){
+
+            System.out.println("nothing");
+        }
+
+        backbBut.setOnMouseClicked( event -> {
+            Difficultymenue(imageView, root3);
+        });
+
+
+        AnchorPane scorePane = new AnchorPane();
+        AnchorPane scorePa= new AnchorPane();
+        HBox scoreBox = new HBox(sCORE,progressBar);
+        scoreBox.setAlignment(Pos.TOP_CENTER);
+        scoreBox.setSpacing(10);
+
+        HBox scoreB = new HBox(backbBut);
+        scoreB.setAlignment(Pos.TOP_LEFT);
+        scoreB.setSpacing(10);
+        
+        // Set top anchor for the HBox to position it at the top
+        AnchorPane.setTopAnchor(scoreBox, 40.0);
+        
+        
+        // Set left and right anchors for the HBox to stretch it horizontally
+        AnchorPane.setLeftAnchor(scoreBox, 0.0);
+        AnchorPane.setRightAnchor(scoreBox, 0.0);
+
+        AnchorPane.setTopAnchor(scoreB, 40.0);
+    
+        // Set left and right anchors for the HBox to stretch it horizontally
+        AnchorPane.setLeftAnchor(scoreB, 20.0);
+        AnchorPane.setRightAnchor(scoreB, 0.0);
+        
+
+        scorePane.getChildren().add(scoreBox);
+        scorePa.getChildren().add(scoreB);
+        Image moleImage = img; 
+        // Image hammerImage = new Image("mole.png"); // Replace "hammer.png" with your actual hammer image file
+        // ImageView hammerImageView = new ImageView(hammerImage);
+        // hammerImageView.setVisible(false);
+
 
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
@@ -199,27 +286,42 @@ public class whackamole extends Application {
                 holeImageView.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-border-style: solid; -fx-max-width: 100%;");
                 holeImageView.fitWidthProperty().bind(inrect.widthProperty().multiply(0.25).subtract(inrect.widthProperty().multiply(0.056)));
                 holeImageView.fitHeightProperty().bind(inrect.heightProperty().multiply(0.36).subtract(inrect.heightProperty().multiply(0.03)).divide(2));
-                
-               
-                
 
                  moles[row][col] = holeImageView;
                  gridPane.add(holeImageView, col, row); 
-                    GridPane.setMargin(holeImageView, new Insets(0, 0,0 , 8));
-            }
-        }
+                    GridPane.setMargin(holeImageView, new Insets(0, 0,0 , 11));
 
-
+                    final int finalRow = row;
+                    final int finalCol = col;
         
-        root5.getChildren().add(sCORE);
-        stackPane.getChildren().addAll(gridPane,gridP);
+                    holeImageView.setOnMouseClicked(mouseEvent -> {
+                        if (moles[finalRow][finalCol].getOpacity() == 1.0) {
+                            scorecount++;  
+                            sCORE.setText("Score: " + scorecount + "/20");
+                            progressBar.setProgress(scorecount / 20.0);
+                            if (scorecount == 20 ){
+                                timeline.stop();
+                            }
+                            holeImageView.setOpacity(0.0);
+
+                                System.out.println("Burger");
+                        
+                        }
+                    });
+                }
+            }
+            VBox vbox = new VBox(gridPane, scorePa);
+            VBox.setMargin(gridPane, new Insets(90, 0, 0, 0));
+            vbox.setAlignment(Pos.CENTER);
+        
+            stackPane.getChildren().addAll(scorePane, vbox);
     
         return stackPane;
     }
     
 
     private void startMoleAnimation() {
-        Timeline timeline = new Timeline();
+
         timeline.setCycleCount(Timeline.INDEFINITE);
 
         KeyFrame appearFrame = new KeyFrame(Duration.millis(MOLE_APPEAR_TIME), event -> {
@@ -227,8 +329,15 @@ public class whackamole extends Application {
             int randomCol = ran.nextInt(BOARD_SIZE);
             ImageView moleImageView = moles[randomRow][randomCol];
             moleImageView.setOpacity(1.0); // Make the mole visible
-            
+        
+            // Add mouse click event handler to the moleImageView
         });
+        
+           KeyFrame countdown = new KeyFrame(Duration.seconds(1), event -> {
+            timerLabel = new Label("0 seconds");
+                seconds++;
+                timerLabel.setText(seconds + " seconds");
+            });
 
         KeyFrame disappearFrame = new KeyFrame(Duration.millis(MOLE_APPEAR_TIME + MOLE_DISAPPEAR_TIME), event -> {
             for (ImageView[] row : moles) {
@@ -239,10 +348,21 @@ public class whackamole extends Application {
         });
 
 
-        timeline.getKeyFrames().addAll(appearFrame, disappearFrame);
+
+        timeline.getKeyFrames().addAll(countdown, appearFrame, disappearFrame);
         timeline.play();
         }
 
+private void mainmenue(ImageView img , VBox vb){
+    stackPane.getChildren().setAll(img,vb);
+ }
+
+private void levelmenue(ImageView vw, VBox jl){
+    stackPane.getChildren().setAll(vw,jl);
+}
+private void Difficultymenue(ImageView kl , VBox hl){
+    stackPane.getChildren().setAll(kl, hl);
+}
 
 
     public static void main(String[] args) {
